7da2caae43ab21a53133a4338161b17b1f642f1f
Hapusny <y2324056693@163.com>
2026-02-04 20:16:08 +0800

    Component

--- Source/UECPP/C_PC.cpp
+++ Source/UECPP/C_PC.cpp
@@ -2,6 +2,20 @@
 
 
 #include "C_PC.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+AC_PC::AC_PC()
+{
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>("PlayerCamera");
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 600.f;
+	CameraBoom->SetRelativeRotation(FRotator(-30.f, 0.f, 0.f));
+	PlayerCamera->SetupAttachment(CameraBoom);
+
+	
+}
 
 void AC_PC::C_PCTest()
 {
--- Source/UECPP/C_PC.h
+++ Source/UECPP/C_PC.h
@@ -6,16 +6,32 @@
 #include "GameFramework/PlayerController.h"
 #include "C_PC.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 /**
  * 
  */
 UCLASS()
+
 class UECPP_API AC_PC : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AC_PC();
 	
 protected:
 
 	UFUNCTION(BlueprintCallable)
 	void C_PCTest();
+
+private:
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USpringArmComponent>CameraBoom;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UCameraComponent>PlayerCamera;
+
 };
