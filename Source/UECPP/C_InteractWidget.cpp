d6ef6707999f91cd8a90734bfdeef42e21f528bc
Hapusny <y2324056693@163.com>
2026-02-17 20:49:36 +0800

    InteractWidget

--- Source/UECPP/C_InteractWidget.cpp
+++ Source/UECPP/C_InteractWidget.cpp
@@ -18,7 +18,77 @@ bool UC_InteractWidget::Initialize()
         Button->OnClicked.AddDynamic(this, &UC_InteractWidget::OnMyButtonClicked);
     }
 
+    IsLeftDown = false;
+
 	return false;
+}
+
+FReply UC_InteractWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+    FReply Reply = Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+
+    // 检查是否是左键按下
+    if (InMouseEvent.IsMouseButtonDown(EKeys::LeftMouseButton))
+    {
+        // 获取鼠标位置
+        FVector2D LocalPosition = InGeometry.AbsoluteToLocal(InMouseEvent.GetScreenSpacePosition());
+
+        // 在这里处理左键按下逻辑
+        UE_LOG(LogTemp, Warning, TEXT("Left mouse button pressed at: %s"), *LocalPosition.ToString());
+
+        IsLeftDown = true;
+        InitPosition = LocalPosition;
+
+        if (CatchCamera)
+        {
+            InitRotator = CatchCamera->GetActorRotation();
+        }
+
+        // 返回表示事件已被处理
+        return FReply::Handled();
+    }
+
+    // 返回未处理，让事件继续传递
+    return FReply::Unhandled();
+}
+
+FReply UC_InteractWidget::NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+    FReply Reply = Super::NativeOnMouseButtonUp(InGeometry, InMouseEvent);
+
+    if (InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Left mouse button released"));
+        IsLeftDown = false;
+        return FReply::Handled();
+    }
+
+    return FReply::Unhandled();
+}
+
+FReply UC_InteractWidget::NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+    FReply Reply = Super::NativeOnMouseMove(InGeometry, InMouseEvent);
+
+    FVector2D LocalPosition = InGeometry.AbsoluteToLocal(InMouseEvent.GetScreenSpacePosition());
+    UE_LOG(LogTemp, Warning, TEXT("Mouse moving at: %s"), *LocalPosition.ToString());
+    if (IsLeftDown)
+    {
+        float NewPicth = InitRotator.Pitch - (LocalPosition - InitPosition).Y;
+        float NewYaw = InitRotator.Yaw + (LocalPosition - InitPosition).X;
+        if (CatchCamera)
+        {
+            CatchCamera->SetActorRotation(FRotator(NewPicth, NewYaw, InitRotator.Roll));
+        }
+        return FReply::Handled();
+    }
+    return FReply::Unhandled();
+}
+
+void UC_InteractWidget::NativeOnMouseLeave(const FPointerEvent& InMouseEvent)
+{
+    Super::NativeOnMouseLeave(InMouseEvent);
+    IsLeftDown = false;
 }
 
 void UC_InteractWidget::OnMyButtonClicked()
--- Source/UECPP/C_InteractWidget.h
+++ Source/UECPP/C_InteractWidget.h
@@ -16,6 +16,9 @@ class UECPP_API UC_InteractWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Catch")
+	TObjectPtr<AActor> CatchCamera;
 
 protected:
 	UPROPERTY(meta = (BindWidget))
@@ -24,8 +27,24 @@ protected:
 	// 初始化时绑定点击事件
 	virtual bool Initialize() override;
 
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+
+	// 可选：重载鼠标释放事件
+	virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+
+	// 可选：重载鼠标移动事件
+	virtual FReply NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+
+	virtual void NativeOnMouseLeave(const FPointerEvent& InMouseEvent) override;
+
 private:
 	// 按钮点击事件的处理函数
 	UFUNCTION()
 	void OnMyButtonClicked();
+
+	bool IsLeftDown;
+
+	FVector2D InitPosition;
+
+	FRotator InitRotator;
 };
