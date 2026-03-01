767bb8523192a604a695d4de0b909c6ee970a6fa
Hapusny <y2324056693@163.com>
2026-03-01 20:01:37 +0800

    StartMatch

---
+++ Source/UECPP/C_MyGM.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_MyGM.h"
+
+AC_MyGM::AC_MyGM()
+{
+	bDelayedStart = true;
+}
+
+void AC_MyGM::BeginPlay()
+{
+	Super::BeginPlay();
+	GetWorldTimerManager().SetTimer(MakeMatchStart, this, &AC_MyGM::StartMatch, 4.f);
+}
+
+void AC_MyGM::StartMatch()
+{
+	Super::StartMatch();
+	GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("StartMatch")));
+}
---
+++ Source/UECPP/C_MyGM.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "C_MyGM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UECPP_API AC_MyGM : public AGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	AC_MyGM();
+
+	virtual void BeginPlay()override;
+
+	virtual void StartMatch()override;
+
+	FTimerHandle MakeMatchStart;
+};
